a6cfcc1cef3aa29c4e64413c3905b4b8b5079d78
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2023-11-27 16:58:29 -0300

    feat: Login method on LoginActivity and intent to it

--- app/src/main/java/com/example/ranchapp/MainActivity.java
+++ app/src/main/java/com/example/ranchapp/MainActivity.java
@@ -25,70 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void login(View view) {
-        // Oculta los botones y el texto existentes
-        View btnLogin = findViewById(R.id.login);
-        View btnAction = findViewById(R.id.Registro);
-        View btnLogout = findViewById(R.id.logout);
-        View text = findViewById(R.id.textView);
-        btnLogin.setVisibility(View.GONE);
-        btnAction.setVisibility(View.GONE);
-        text.setVisibility(View.GONE);
-
-        View imagen = findViewById(R.id.img_principal);
-        imagen.setVisibility(View.GONE);
-        // Crea un nuevo TextView
-        TextView myText = new TextView(view.getContext());
-        myText.setText("Logueate");
-        myText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
-
-        // Crea un nuevo EditText
-        EditText editText = new EditText(view.getContext());
-        editText.setHint("Ingrese su nombre"); // Establece un texto de sugerencia
-        editText.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME); // Establece el tipo de entrada de texto
-
-        // Crea un nuevo EditText para la contraseña
-        EditText passwordEditText = new EditText(view.getContext());
-        passwordEditText.setHint("Ingrese su contraseña"); // Establece un texto de sugerencia para la contraseña
-        passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD); // Establece el tipo de entrada de contraseña
-
-        // Crea un nuevo botón
-        Button confirmButton = new Button(view.getContext());
-        confirmButton.setText("Login"); // Establece el texto del botón
-
-        // Crea un nuevo LinearLayout que actuará como contenedor
-        LinearLayout linearLayout = new LinearLayout(view.getContext());
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        // Agrega el TextView y el EditText al LinearLayout
-        linearLayout.addView(myText);
-        linearLayout.addView(editText);
-        linearLayout.addView(passwordEditText);
-        linearLayout.addView(confirmButton);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myText.setVisibility(View.GONE);
-                editText.setVisibility(View.GONE);
-                passwordEditText.setVisibility(View.GONE);
-                confirmButton.setVisibility(View.GONE);
-                btnLogout.setVisibility(View.VISIBLE);
-                imagen.setVisibility(View.VISIBLE);
-                // btnLogin.setVisibility(View.VISIBLE);
-                // btnAction.setVisibility(View.VISIBLE);
-                text.setVisibility(View.VISIBLE);
-
-            }
-        });
-        // Agrega el LinearLayout a la vista raíz de la actividad
-        ViewGroup rootView = findViewById(android.R.id.content);
-        rootView.addView(linearLayout);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 
     public void singUp(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
-
     }
 
 
