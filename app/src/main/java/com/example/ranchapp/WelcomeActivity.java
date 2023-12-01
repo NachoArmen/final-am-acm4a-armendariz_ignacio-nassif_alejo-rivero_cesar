7688b0509c5300776bcba805f9d88cc3bc9ba44d
CesaRivero <cesar_tomas_rivero@hotmail.com>
2023-12-01 14:47:53 -0300

    create: welcome_page

--- app/src/main/java/com/example/ranchapp/WelcomeActivity.java
+++ app/src/main/java/com/example/ranchapp/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.ranchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //hacer los checkbox o la lista de los eventos y un boton de crear, al darle crear te debe mostrar los enventos creados
     }
+
+    public void create_event(View view){
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        startActivity(intent);
+    }
+    public void logOut(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
