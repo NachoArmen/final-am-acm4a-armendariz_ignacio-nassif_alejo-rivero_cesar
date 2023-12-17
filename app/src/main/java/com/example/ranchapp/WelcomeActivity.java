eb0d9c55141f5c9b95a7fc7888fca1e44f518ead
alenassif <alejo.nassif@davinci.edu.ar>
2023-12-17 19:14:18 -0300

    Descarga por URL
    Creamos metodo en Onstart para que se descargue imagen en segundo plano

--- app/src/main/java/com/example/ranchapp/WelcomeActivity.java
+++ app/src/main/java/com/example/ranchapp/WelcomeActivity.java
@@ -1,17 +1,23 @@
 package com.example.ranchapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -35,8 +41,47 @@ public class WelcomeActivity extends AppCompatActivity {
         FirebaseUser currentUser = this.mAuth.getCurrentUser();
         TextView msg = findViewById(R.id.msgBienvenida);
         msg.setText("Ha ingresado con " + currentUser.getEmail());
+        // Definimos url de la imagen
+        String imageUrl = "https://www.telam.com.ar/thumbs/bluesteel/advf/imagenes/2020/06/5ed4e9b33f900_450.jpg";
 
+        // Creamos imageView para mostrar imagen
+        ImageView imageView = findViewById(R.id.imageView);
+
+        // Creamos metodo para cargar la imagen en segundo plano
+        private void loadImage() {
+
+
+            URLConnection connection = new URL(imageUrl).openConnection();
+
+
+            connection.setConnectTimeout(3000);
+            connection.setReadTimeout(3000);
+
+
+            byte[] imageData = new byte[connection.getContentLength()];
+            try {
+                connection.getInputStream().read(imageData);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Convertimos imagen a bitmap
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+
+            // Seteamos imagen a imageview
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+            } else {
+                imageView.setImageResource(R.drawable.error);
+            }
+        }
+
+        // Cargar imagen en el fondo
+        loadImage();
     }
+
+
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -86,6 +131,10 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
+    }
+    public void downloadImage(View v){
+        DownloadImage image = new DownloadImage();
+        image.execute("https://www.telam.com.ar/thumbs/bluesteel/advf/imagenes/2020/06/5ed4e9b33f900_450.jpg");
     }
 
 }
